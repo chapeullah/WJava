0bd36c4c80b704a415372a90e3085e7461f72dcf
chapeullah <chapeullah@gmail.com>
2025-07-19 11:14:14 +0300

    Update files

--- Projects/ExpenseTracker/src/main/java/com/chapeullah/ExpenseTracker.java
+++ Projects/ExpenseTracker/src/main/java/com/chapeullah/ExpenseTracker.java
@@ -1,25 +1,54 @@
 package com.chapeullah;
 
-import java.util.Scanner;
-
 import picocli.CommandLine;
+import picocli.CommandLine.*;
 
-public class ExpenseTracker {
+@Command(name = "expense-tracker")
+public class ExpenseTracker implements Runnable {
+
+    @Parameters(index = "0")
+    private String command;
+
+    @Option(names = "--description")
+    private String description;
+
+    @Option(names = "--amount")
+    private Double amount;
+
+    @Option(names = "--id")
+    private Integer id;
+
+    @Option(names = "--mounth")
+    private Integer month;
+
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new ExpenseTracker()).execute(args);
+        System.exit(exitCode);
+    }
 
-        String in = null;
-
-        while (in == null) {
-            System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
-            in = scanner.nextLine();
-            switch (in) {
-                default -> {
-                    System.out.println("Unknown command!\n");
-                    in = null;
-                }
-            }
+    public void run() {
+        switch (command) {
+            case "add" -> addCommand();
+            case "list" -> listCommand();
+            case "delete" -> deleteCommand();
+            case "summary" -> summaryCommand();
         }
+    }
+
+    private void addCommand() {
 
     }
+
+    private void listCommand() {
+
+    }
+
+    private void deleteCommand() {
+
+    }
+
+    private void summaryCommand() {
+        
+    }
+
 }
