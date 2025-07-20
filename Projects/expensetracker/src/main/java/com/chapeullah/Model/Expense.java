735924ce012ae253b3d0097698664dabe3889c29
chapeullah <chapeullah@gmail.com>
2025-07-20 13:03:48 +0300

    Update files

---
+++ Projects/expensetracker/src/main/java/com/chapeullah/Model/Expense.java
@@ -0,0 +1,35 @@
+package com.chapeullah.Model;
+
+import java.time.LocalDate;
+
+public class Expense {
+
+    private int id;
+    private String description;
+    private double amount;
+    private String date;
+
+    public Expense(int id, String description, double amount, LocalDate date) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+        this.date = date.toString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+}
---
+++ Projects/expensetracker/src/main/java/com/chapeullah/StorageManager.java
@@ -0,0 +1,99 @@
+package com.chapeullah;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import com.chapeullah.Model.Expense;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class StorageManager {
+
+    private static int id = 1;
+    private static final File expensesFile = new File("expenses.json");
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private static ArrayList<Expense> expenses;
+
+    public void addExpense(String description, double amount) {
+        try {
+            if (expensesFile.exists()) {
+                expenses = objectMapper.readValue(
+                    expensesFile, new TypeReference<>() {}
+                );
+            }
+            else {
+                expenses = new ArrayList<>();
+            }
+            expenses.add(new Expense(id, description, amount, LocalDate.now()));
+            objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValue(expensesFile, expenses);
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public boolean removeExpense(int id) {
+        try {
+            if (!expensesFile.exists()) {
+                return false;
+            }
+            expenses = objectMapper.readValue(
+                expensesFile, new TypeReference<>() {}
+            );
+            if (!expenses.removeIf(expense -> expense.getId() == id)) {
+                return false;
+            }
+            objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValue(expensesFile, expenses);
+
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public double getSummary(int month) {
+        double summary = 0;
+        try {
+            expenses = objectMapper.readValue(
+                expensesFile, new TypeReference<>() {}
+            );
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        if (month != -1) {
+            for (Expense expense : expenses) {
+                if (LocalDate.parse(expense.getDate()).getMonthValue() == month) {
+                    summary += expense.getAmount();
+                }
+            }
+        }
+        else {
+            for (Expense expense : expenses) {
+                summary += expense.getAmount();
+            }
+        }
+        return summary;
+    }
+
+    public ArrayList<Expense> getExpenses() {
+        try {
+            expenses = objectMapper.readValue(
+                expensesFile, new TypeReference<>() {}
+            );
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+            return new ArrayList<>();
+        }
+        return expenses;
+    }
+
+}
